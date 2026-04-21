427a839deac5e967a49a77424aefb8dc357a39eb
Krzysztof <klachowskia@gmail.com>
2026-04-21 20:53:45 +0200

    Dodanie modeli Rezerwacji oraz pokoj

---
+++ Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; }
+
+    [Required]
+    public string Topic { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    public string Status { get; set; }
+}
